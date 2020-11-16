88f6dd0670c9077fffeba7d55cdd817932c5211d
Laszlo-Andras Zsurzsa <zsurzsalaszlo@gmail.com>
2020-11-16 08:57:25 +0100

    Add gallery controller.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import ImageService from './application/image.service';
 import { ImageController } from './handler/image.controller';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { GalleryController } from './handler/gallery.controller';
 
-@Module({
-  imports: [],
-  controllers: [AppController, ImageController],
+Module({
+  imports: 
+  [ 
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+      exclude: ['/api*'],
+    }),
+  ],
+  controllers: [
+    AppController,
+    ImageController,
+    GalleryController
+  ],
   providers: [AppService, ImageService],
 })
 export class AppModule {}
