f6a1991be70fd5587b08de108ac062b4870209fb
Laszlo-Andras Zsurzsa <zsurzsalaszlo@gmail.com>
2020-11-16 08:57:25 +0100

    Add gallery controller.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express/interfaces/nest-express-application.interface';
 import { AppModule } from './app.module';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname, '..', 'ui'));
+  app.setViewEngine('html');
+
   await app.listen(3000);
 }
 bootstrap();
