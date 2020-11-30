68a54375ba9d8c88b32372ca3d3e2db61ac86b53
Laszlo-Andras Zsurzsa <zsurzsalaszlo@gmail.com>
2020-12-01 00:44:09 +0100

    Try out and read folder structure using internal node commands.wq

---
+++ public/js/main.js
@@ -0,0 +1,28 @@
+// Get the modal
+const modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+const img = document.getElementById("myImg");
+if (img) {
+    const modalImg = document.getElementById("img01");
+    const captionText = document.getElementById("caption");
+
+    img.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+    }
+}
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+if (span) {
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+}
+
+function goToGallery(){
+    location.href='gallery.html';
+}
--- src/index.js
+++ src/index.js
@@ -1,27 +1,15 @@
-// Get the modal
-const modal = document.getElementById("myModal");
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-const img = document.getElementById("myImg");
-const modalImg = document.getElementById("img01");
-const captionText = document.getElementById("caption");
-
-img.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-}
-
-// Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
 import { AssetTree } from './AssetTree';
-import { goToGallery } from './goToGallery';
+var fs = require('fs');
 
 let assetTree = new AssetTree(10,10);
-console.log(assetTree.getValues);
+console.log(assetTree.getValues());
+
+const directoryToScan = __dirname + '/../public/img/root';
+
+fs.readdir(directoryToScan, (err, files) => {
+    if (files) {
+        files.forEach(file => {
+            console.log(file);
+        });
+    }
+});
