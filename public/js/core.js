93f7d1fe3181d2280a526c7c27338396da091ac7
Laszlo-Andras Zsurzsa <zsurzsalaszlo@gmail.com>
2020-12-03 12:57:39 +0100

    Basic logging.

---
+++ public/js/core.js
@@ -0,0 +1,19 @@
+let application = (function(){
+    'use strict'
+
+    // Public methods object
+    let methods = {};
+
+
+	/**
+	 * Extend the public methods object
+	 * @param  {String}   name The new method name
+	 * @param  {Function} fn   The new method
+	 */
+	methods.extend = function (name, fn) {
+		methods[name] = fn;
+	};
+
+	// Return public methods object
+	return methods;
+})();
---
+++ public/js/settings.js
@@ -0,0 +1,44 @@
+// Settings functionality
+(function () {
+
+	'use strict';
+
+	// Create app settings
+    let settings = {
+        debug: true
+    };
+
+    /**
+     * Update the settings object
+     * @param  {String} key The setting key
+     * @param  {*}      val The new value
+     */
+    let setting = function (key, val) {
+
+        // if the setting doesn't exist, bail
+        if (!(key in settings)) return;
+
+        // Update the settings
+        settings[key] = val;
+    };
+
+    /**
+     * Get settings
+     * @param  {String} key The setting key (optional)
+     * @return {*}          The setting or object of settings
+     */
+    var getSettings = function (key) {
+
+        // If there's a key, get a specific setting
+        if (key) {
+            return settings[key];
+        }
+
+        // Otherwise return the whole settings object
+        return Object.assign({}, settings);
+    };
+
+    // setup front-end application
+    application.extend('setting', setting);
+    application.extend('getSettings', getSettings);
+})();
