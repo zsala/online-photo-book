0f2ff64e4e63b7e1cf76b6e5b56fa8b4198bea03
Laszlo-Andras Zsurzsa <zsurzsalaszlo@gmail.com>
2020-12-20 22:41:30 +0100

    Update generator commands.

--- webpack.js
+++ webpack.js
@@ -6,7 +6,7 @@ module.exports = {
   mode: "production",
   entry: "./src/index.js",
   output: {
-    filename: "[name].bundle.js",
+    filename: "[name].generator.js",
     path: path.resolve(__dirname, "dist")
   },
   devServer: {
